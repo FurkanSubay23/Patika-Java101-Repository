79979b7eddb5d7959d54e0a81ea964c16e1ffc12
Furkan Subay <subayfs@gmail.com>
2022-07-13 23:42:23 +0300

    bolunenveteksayi

---
+++ src/bolunen.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class bolunen {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Bir sayi giriniz");
+        int k= scan.nextInt();
+        int a=0, b=0, c = 0;
+        for (int i=0; i<=k; i++){
+            System.out.println(i);
+
+        }
+        System.out.println("------------------------");
+        System.out.println("3 e ve 4 e bolunen sayilar");
+        for (int i =1; i<=k; i++){
+            if (i % 3 == 0 && i % 4 == 0){
+                System.out.println(i);
+                a+=i;
+                b++;
+            }
+        }
+        System.out.println("-----------------------");
+        c = a /b;
+        System.out.println(c);
+
+    }
+}
+
