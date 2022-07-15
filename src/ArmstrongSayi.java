929b273304742adad7f51fbba21b4b09f0da94cd
Furkan Subay <subayfs@gmail.com>
2022-07-15 12:43:52 +0300

    Armstrong

--- src/ArmstrongSayi.java
+++ src/ArmstrongSayi.java
@@ -2,22 +2,41 @@ import java.util.Scanner;
 public class ArmstrongSayi {
     public static void main(String[] args) {
         //ArmStrong Sayi Bulma
-        int sayi,k=0,total,sanal,sayac=0;
+        int sayi,sanal,sayac=0,total,basamak = 1,usler = 0,k=1;
         Scanner scan = new Scanner(System.in);
         do {
             System.out.println("Bir sayi gir");
-            sayi = scan.nextInt();
+            sayi = scan.nextInt();               // Sayi alindi.
             sanal = sayi;
-        }while (sayi <=0);      // Sayi alindi.
+        }while (sayi <=0);
 
         while (sanal !=0){
-            sayac = (sanal % 10);
-            k+=sayac;
+            total = (sanal % 10);
             sanal = sanal / 10;
-            System.out.print(" "+sayac);
+            System.out.print(" "+total);
+            sayac++;
         }
         System.out.println();
-        System.out.println(k);
+        System.out.println(sayac);
+
+        sanal = sayi;
+        while (sanal !=0){
+            basamak = (sanal % 10);
+            sanal /=10;
+            k=1;
+            for (int i =1; i<=sayac; i++){
+                k*=basamak;
+            }
+            System.out.println(k);
+            usler += k;
+        }
+        System.out.println(usler);
+        if (usler==sayi){
+            System.out.println(sayi + " Sayisi Armstrong sayisidir");
+        }
+        else {
+            System.out.println(sayi+" sayisi armstrong sayisi degildir.");
+        }
 
 
     }
