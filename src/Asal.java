ff9e66079c7082ddd372260808b6120e70e35ed4
Furkan Subay <subayfs@gmail.com>
2022-07-17 02:30:55 +0300

    asal

---
+++ src/Asal.java
@@ -0,0 +1,18 @@
+public class Asal {
+    public static void main(String[] args) {
+        System.out.println("2");
+        for (int i=3; i<=100;i++){
+            boolean var =false;
+            for (int j=2; j<i; j++){
+                if (i % j ==0){
+                    var = true;
+                    break;
+                }
+            }
+            if (var==false){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
