7dadc20a52fec0513c907f634dee27383a2194f1
Furkan Subay <subayfs@gmail.com>
2022-07-16 18:13:26 +0300

    ATM new

--- src/ATM.java
+++ src/ATM.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class ATM {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        Scanner scan=new Scanner(System.in);
-        int bakiye=150,islem,cek,yatir;
-        String id,password,idd,pasw;
+        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        int bakiye = 150, islem, cek, yatir,toplam = 0;
+        String id, password, idd, pasw, devam;
         System.out.println("Id nizi belirleyiniz");
         id = scan.nextLine();
         System.out.println("Password belirleyin");
@@ -13,40 +13,77 @@ public class ATM {
         System.out.println("ID girin");
         idd = scan.nextLine();
         System.out.println("passwordunuzu girin");
-        pasw =scan.nextLine();
-        while (!id.equals(idd) || !password.equals(pasw) ){
+        pasw = scan.nextLine();
+        while (!id.equals(idd) || !password.equals(pasw)) {
             System.out.println("ID veya sifreniz yanlistir. Bir daha giriniz");
             System.out.println("ID");
             idd = scan.nextLine();
             System.out.println("passwordunuzu girin");
-            pasw =scan.nextLine();
-        }                                               // Switch case yapısını while içine yazmıyorurzki takını olmasın.
+            pasw = scan.nextLine();
+        }                                               // Switch case yapısını while içine yazmıyorurz ki sıkıntı olmasın.
+        System.out.println("Bir islem seciniz");
+        System.out.println("1 Bakiye sorgula\n2 para cek\n3 para yatir");
+        islem = input.nextInt();
+        while (islem < 1 || islem > 3) {
+            System.out.println("Yanlis secim yapildi bir daha");
             System.out.println("Bir islem seciniz");
             System.out.println("1 Bakiye sorgula\n2 para cek\n3 para yatir");
             islem = input.nextInt();
-            while (islem<1 || islem>3) {
+        }
+        switch (islem) {
+            case 1:
+                System.out.println(bakiye + " Tl niz vardir");
+                break;
+            case 2:
+                System.out.println("Kac tl cekeceksiniz ?");
+                cek = input.nextInt();
+               bakiye -=cek;
+                System.out.println("Kalan bakiyeniz: " + bakiye);
+                break;
+            case 3:
+                System.out.println("Yatirmak istediginiz parayi giriniz");
+                yatir = input.nextInt();
+                bakiye+=yatir;
+                System.out.println("Toplam paraniz: " + bakiye);
+                break;
+        }
+
+        System.out.println("Baska bir islem yapmak istiyor musunuz ?");
+        devam = scan.nextLine();
+        while (devam.equals("evet")) {
+            System.out.println("Bir islem seciniz");
+            System.out.println("1 Bakiye sorgula\n2 para cek\n3 para yatir");
+            islem = input.nextInt();
+            while (islem < 1 || islem > 3) {
                 System.out.println("Yanlis secim yapildi bir daha");
                 System.out.println("Bir islem seciniz");
                 System.out.println("1 Bakiye sorgula\n2 para cek\n3 para yatir");
                 islem = input.nextInt();
             }
-            switch (islem){
+            switch (islem) {
                 case 1:
-                    System.out.println(bakiye +" Tl niz vardir");
+                    System.out.println(bakiye + " Tl niz vardir");
                     break;
                 case 2:
                     System.out.println("Kac tl cekeceksiniz ?");
                     cek = input.nextInt();
-                    System.out.println("Kalan bakiyeniz: "+(bakiye-cek));
+                    bakiye = bakiye - cek;
+                    System.out.println("Kalan bakiyeniz: " + bakiye);
                     break;
                 case 3:
                     System.out.println("Yatirmak istediginiz parayi giriniz");
                     yatir = input.nextInt();
-                    System.out.println("Toplam paraniz: "+(bakiye+yatir));
-                default:
-                    System.out.println("Yanlis secim yaptiniz bir daha seciniz.");
+                    bakiye = bakiye + yatir;
+                    System.out.println("Toplam paraniz: " + bakiye);
+                    break;
             }
+            System.out.println("Baska bir islem yapmak istiyor musunuz ?");
+            devam = scan.nextLine();
+
+        }
+    }
+
 
 
     }
-}
+
