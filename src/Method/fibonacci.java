31127c962ff0e0eee4f67a95fc1ab039de6400e9
Furkan Subay <subayfs@gmail.com>
2022-07-18 22:12:06 +0300

    fibonacci recursive

---
+++ src/Method/fibonacci.java
@@ -0,0 +1,15 @@
+package Method;
+import java.util.Scanner;
+public class fibonacci {
+
+    static int fibonacci(int sayi){
+       if ( sayi == 1 || sayi == 2 ){
+           return 1;
+       }
+       return fibonacci(sayi - 1) + fibonacci(sayi - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+    }
+}
