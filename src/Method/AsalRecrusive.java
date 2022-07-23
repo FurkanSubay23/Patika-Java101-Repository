c6b414121fe879f036f912521fc8806361433b85
Furkan Subay <subayfs@gmail.com>
2022-07-24 02:07:09 +0300

    asalRecrusive

---
+++ src/Method/AsalRecrusive.java
@@ -0,0 +1,30 @@
+package Method;
+import java.util.Scanner;
+public class AsalRecrusive {
+
+    static void asalRecrusive(int a,int i){
+        if (a == i){
+            System.out.println(a + " number is asal");
+           return;
+        } else if (a % i == 0 ) {
+            System.out.println(a + " number is  not asal");
+            return;
+        }
+        asalRecrusive(a,i + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        boolean var = true;
+        while (var){
+            System.out.println("Enter the number: ");
+            int a = input.nextInt();
+            asalRecrusive(a,2);
+            System.out.println("Dou you want to continue: ");
+            String cnt = scan.nextLine();
+            if (cnt.equals("no"))
+                var = false;
+        }
+    }
+}
