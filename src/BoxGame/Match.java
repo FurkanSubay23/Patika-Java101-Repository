4c484d939004b4b6d482ecc825efd4b062ed8e9f
Furkan Subay <subayfs@gmail.com>
2022-07-25 21:46:04 +0300

    for push

--- src/BoxGame/Match.java
+++ src/BoxGame/Match.java
@@ -12,22 +12,6 @@ public class Match {
         this.minWeight = minWeight;
         this.maxWeight = maxWeight;
     }
-
-    void run() {
-        if ((f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight)){
-            if (isFirst()){
-                System.out.println("----------NEW ROUND----------");
-                this.f2.health = this.f1.hit(this.f2);
-                System.out.println(this.f2.name + " isimli kisinin " + this.f2.health + " cani kaldi");
-            }
-            else  {
-                System.out.println("----------NEW ROUND----------");
-                this.f1.health = this.f2.hit(this.f1);
-                System.out.println(this.f1.name + " isimli kisinin " + this.f1.health + " cani kaldi");
-            }
-        }
-    }
-
     void run1() {
         if ((f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight)) {
             if (isFirst()){                                                                                             // İlk vuruş için random üretiliyor.
