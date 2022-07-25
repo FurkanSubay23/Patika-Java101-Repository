6d19a8729a986bcb146a6a253d3a382fb9950721
Furkan Subay <subayfs@gmail.com>
2022-07-26 01:55:34 +0300

    maasHesaplandi

---
+++ src/MaasHesaplayici/Main.java
@@ -0,0 +1,10 @@
+package MaasHesaplayici;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Furkan", 1453, 48, 2012);
+        emp1.printEmployee();
+        Employee emp2 = new Employee("Faruk", 1071, 37, 2018);
+        emp2.printEmployee();
+    }
+}
