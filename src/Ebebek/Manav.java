b341afb363098204dc6d327fffd9cfaf65387356
Furkan Subay <subayfs@gmail.com>
2022-11-08 00:21:31 +0300

    manav1

--- src/Ebebek/Manav.java
+++ src/Ebebek/Manav.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Manav {
     public static void main(String[] args) {
-        Double armutKilo=2.14,elmaKilo=3.67,domatesKilo=1.11,muzKilo=0.95,patlicanKilo=5.00;
-        double armutMiktar,elmaMiktari,domatesMiktar,muzMiktar,patlicanMiktar;
+        Double pearKg =2.14, appleKg =3.67, tomatoKg =1.11, bananaKg =0.95, aubergineKg =5.00;
+        double pearValue, appleValue, tomatoValue, bananaValue, aubergineValue;
         Scanner inp=new Scanner(System.in);
         System.out.print("Armut KG:");
-        armutMiktar=inp.nextDouble();
+        pearValue =inp.nextDouble();
         System.out.print("Elma KG:");
-        elmaMiktari=inp.nextDouble();
+        appleValue =inp.nextDouble();
         System.out.print("Domates KG:");
-        domatesMiktar=inp.nextDouble();
+        tomatoValue =inp.nextDouble();
         System.out.print("Muz KG:");
-        muzMiktar=inp.nextDouble();
+        bananaValue =inp.nextDouble();
         System.out.print("Patlican KG:");
-        patlicanMiktar=inp.nextDouble();
-        double toplamTutar=(armutKilo*armutMiktar)+(elmaKilo+elmaMiktari)+
-                (domatesKilo+domatesMiktar)+(muzKilo+muzMiktar)+(patlicanKilo+patlicanMiktar);
-        System.out.print("TOPLAM :"+toplamTutar);
+        aubergineValue =inp.nextDouble();
+        double result =(pearKg * pearValue)+(appleKg + appleValue)+
+                (tomatoKg + tomatoValue)+(bananaKg + bananaValue)+(aubergineKg + aubergineValue);
+        System.out.print("TOPLAM :"+ result);
 
     }
 }
