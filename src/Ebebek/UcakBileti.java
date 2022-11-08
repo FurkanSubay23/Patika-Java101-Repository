920920e0b50c5a8f62a2841173c2ffb2bcbc2b07
Furkan Subay <subayfs@gmail.com>
2022-11-08 23:52:51 +0300

    ucak1

--- src/Ebebek/UcakBileti.java
+++ src/Ebebek/UcakBileti.java
@@ -21,33 +21,33 @@ public class UcakBileti {
         System.out.print("Lütfen Yaşınızı giriniz:");
         old = input.nextInt();
 
-        if (type == 1) {
+        if (type == 1) {    //if customer select type 1
 
-            if ((old >= 0) && (old < 12)) {
+            if ((old >= 0) && (old < 12)) {              //if old bigger than 0 AND smaller than 12
                 price = pricePerKM * 0.5;
                 System.out.print("Ödeyeceğiniz Tutar: " + price + "TL");
-            } else if ((old >= 12) && (old <= 24)) {
+            } else if ((old >= 12) && (old <= 24)) {    //if old bigger than 12 AND smaller than 24
                 price = pricePerKM * 0.9;
                 System.out.print("Ödeyeceğiniz Tutar: " + price + "TL");
 
-            } else if (old >= 65) {
+            } else if (old >= 65) {                     //if old bigger than 65
                 price = pricePerKM * 0.7;
                 System.out.print("Ödeyeceğiniz Tutar: " + price + "TL");
             } else {
                 System.out.print("Hatalı İşlem Yapıldı! Lütfen Tekrar Deneyiniz.");
             }
 
-        } else if (type == 2) {
+        } else if (type == 2) { //if customer choose type 2
 
-            if ((old >= 0) && (old < 12)) {
+            if ((old >= 0) && (old < 12)) {     //if old bigger than 0 AND smaller than 12
                 price = (2 * pricePerKM) * 0.5 * 0.8;
                 System.out.print("Ödeyeceğiniz Tutar: " + price + "TL");
 
-            } else if ((old >= 12) && (old <= 24)) {
+            } else if ((old >= 12) && (old <= 24)) {    //if old bigger than 12 AND smaller than 24
                 price = (2 * pricePerKM) * 0.9 * 0.8;
                 System.out.print("Ödeyeceğiniz Tutar: " + price + "TL");
 
-            } else if (old >= 65) {
+            } else if (old >= 65) {                     //if old bigger than 64
                 price = (2 * pricePerKM) * 0.7 * 0.8;
                 System.out.print("Ödeyeceğiniz Tutar: " + price + "TL");
             } else {
