81cff0f5e768cdcf8623967a731eadf29c5fa1ec
Furkan Subay <subayfs@gmail.com>
2022-11-09 00:43:55 +0300

    asal

---
+++ src/Donguler/AsalFind.java
@@ -0,0 +1,19 @@
+package Donguler;
+
+public class AsalFind {
+    public static void main(String[] args) {
+        //System.out.println("2");
+        for (int i = 2; i <= 100; i++) {    // 2 ile 100 arası dedıgı ıcın basa o sayıları getiren for dongusu
+            boolean var = false;
+            for (int j = 2; j < i; j++) {   // sonra o gelen sayıyı bolunuyor mu diye o sayıya kadarkı sayıları bolduruyoruz
+                if (i % j == 0) {
+                    var = true;
+                    break;
+                }
+            }
+            if (var == false) {     // eger bolunmemişse asal sayıdır dıyıp yazdırıyoruz.
+                System.out.println(i);
+            }
+        }
+    }
+}
