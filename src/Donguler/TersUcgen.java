c75cc420cae5dc16fadbac99056999a74092acd3
Furkan Subay <subayfs@gmail.com>
2022-11-09 00:26:45 +0300

    ters1

--- src/Donguler/TersUcgen.java
+++ src/Donguler/TersUcgen.java
@@ -1,16 +1,19 @@
+package Donguler;
+
 import java.util.Scanner;
+
 public class TersUcgen {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Bir sayi gir");
         int sayi = scan.nextInt();
-        for (int i=sayi; i>=0; i--){
-            for (int j=sayi-i; j>0; j--){
+        for (int i = sayi; i >= 0; i--) {   // girilen sayidan 0 a kadar git.
+            for (int j = sayi - i; j > 0; j--) {    // girilen sayidan i yi çıkar ve j 0 olana kadar boşluk bırak
                 System.out.print(" ");
             }
-           for (int k=(2*i)+1; k>0; k--){
-               System.out.print("*");
-           }
+            for (int k = (2 * i) + 1; k > 0; k--) { // boşlukdan sonra i nin 2 katının 1 fazlasından 0 akadar * bırak.
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
