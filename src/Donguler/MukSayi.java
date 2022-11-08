8bde88e5069133eda514a48a7b72cf08177003b9
Furkan Subay <subayfs@gmail.com>
2022-11-09 00:22:13 +0300

    mukkemmelsayi

--- src/Donguler/MukSayi.java
+++ src/Donguler/MukSayi.java
@@ -1,20 +1,22 @@
+package Donguler;
+
 import java.util.Scanner;
+
 public class MukSayi {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        int sayac = 0;
+        Scanner input = new Scanner(System.in);
+        int counter = 0;
         System.out.println("bir sayi gir");
         int sayi = input.nextInt();
-        for (int i=1; i<sayi; i++){
-            if (sayi % i ==0){
-                sayac+=i;
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {    //girilen sayının i'ye bölumunden kalan 0 ise sayaca i'yi ekle.
+                counter += i;
             }
         }
-        if (sayac == sayi){
+        if (counter == sayi) {  // sayac girilen sayıya esıt ise mukemmel sayıdır.
             System.out.println(sayi + " Muko sayisidir.");
-        }
-        else {
-            System.out.println(sayi +" Muko degildir.");
+        } else {
+            System.out.println(sayi + " Muko degildir.");   //Değilse mukemmel sayı degıldır.
         }
 
     }
