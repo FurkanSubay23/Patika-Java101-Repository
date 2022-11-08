80ba73aa0d8d1fd6ba1b198945dade0b33911e0a
Furkan Subay <subayfs@gmail.com>
2022-11-09 00:30:23 +0300

    asal1

--- src/Donguler/Asal.java
+++ src/Donguler/Asal.java
@@ -2,16 +2,16 @@ package Donguler;
 
 public class Asal {
     public static void main(String[] args) {
-        System.out.println("2");
-        for (int i=3; i<=100;i++){
-            boolean var =false;
-            for (int j=2; j<i; j++){
-                if (i % j ==0){
+        // System.out.println("2");
+        for (int i = 4; i <= 100; i++) {    // 4 ile 100 arası dedıgı ıcın basa o sayıları getiren for dongusu
+            boolean var = false;
+            for (int j = 2; j < i; j++) {   // sonra o gelen sayıyı bolunuyor mu diye o sayıya kadarkı sayıları bolduruyoruz
+                if (i % j == 0) {
                     var = true;
                     break;
                 }
             }
-            if (var==false){
+            if (var == false) {     // eger bolunmemişse asal sayıdır dıyıp yazdırıyoruz.
                 System.out.println(i);
             }
         }
