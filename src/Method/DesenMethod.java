1c6ccdbca13b2452967667bc6dd42e4bc3dd7387
Furkan Subay <subayfs@gmail.com>
2022-11-09 00:49:23 +0300

    desen1

--- src/Method/DesenMethod.java
+++ src/Method/DesenMethod.java
@@ -1,4 +1,5 @@
 package Method;
+
 import java.util.Scanner;
 
 public class DesenMethod {
@@ -6,21 +7,21 @@ public class DesenMethod {
     static void desen(int a) {
         int b = a;
         System.out.print(b);
-        while (b > 0) {
-            b = b - 5;
+        while (b > 0) { // b 0 dan buyuk ise b den 5 cıkar dıyoruz ve yazdırıyoruz
+            b -= 5;
             System.out.print(" " + b);
         }
-        while (b != a) {
-            b = b + 5;
+        while (b != a) {    // b a ya eşit degılse b ye 5 ekle diyoruz ve b yı yazdırıyoruz.
+            b += 5;
             System.out.print(" " + b);
         }
     }
 
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int number = input.nextInt();
-        desen(number);
+        desen(number);  // girdigimiz sayıyı methodda yerine koyuyoruz.
 
     }
 }
