3766a8470e3b8cb75553b32c55e14da1dfb0e7eb
Furkan Subay <subayfs@gmail.com>
2022-11-09 00:40:36 +0300

    us1

--- src/Donguler/UsluSayi.java
+++ src/Donguler/UsluSayi.java
@@ -1,14 +1,17 @@
+package Donguler;
+
 import java.util.Scanner;
+
 public class UsluSayi {
     public static void main(String[] args) {
-        int n,k,total=1;
-        Scanner scan=new Scanner(System.in);
+        int n, k, total = 1;
+        Scanner scan = new Scanner(System.in);
         System.out.println("Tabani gir");
-        n= scan.nextInt();
+        n = scan.nextInt();
         System.out.println("Ussu gir");
-        k= scan.nextInt();
-        for (int i=1; i<=k; i++){
-            total*=n;
+        k = scan.nextInt();
+        for (int i = 1; i <= k; i++) {      // 1 den başla usse kadar git
+            total *= n; // total sayıyı her seferinde taban sayi ile çarp
         }
         System.out.println(total);
     }
