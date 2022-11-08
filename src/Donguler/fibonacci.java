f8c28e28c0a602ee316d67af4260723130c70607
Furkan Subay <subayfs@gmail.com>
2022-11-09 00:38:51 +0300

    fibonacci1

--- src/Donguler/fibonacci.java
+++ src/Donguler/fibonacci.java
@@ -1,16 +1,19 @@
+package Donguler;
+
 import java.util.Scanner;
+
 public class fibonacci {
     public static void main(String[] args) {
-        int n1=1,n2=0,toplam=0;
+        int n1 = 1, n2 = 0, result = 0;
         System.out.println("sayi gir");
-        Scanner scan=new Scanner(System.in);
-        int sayi= scan.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int sayi = scan.nextInt();
         System.out.print("0");
-        for (int i=0; i<sayi; i++){
-          toplam = n1 +n2;
-          n2 =n1;
-          n1=toplam;
-            System.out.print(","+n1);
+        for (int i = 0; i < sayi; i++) {    //0 dan başla sayıya kadar git.
+            result = n1 + n2;               // sonucu önceki 2 sayı ile topla
+            n2 = n1;                        //kucuk sayıyı buyuk sayıya esıtle
+            n1 = result;                    //Buyuk sayıyı sonuca esıtle.
+            System.out.print("," + n1);
         }
     }
 }
